09eb2970d3bbadf70ccef83352ad41ff47ec44f9
langve <langve0+github@gmail.com>
2018-07-04 22:08:47 +0900

    #1 프로젝트 생성

---
+++ src/main/java/studygroup/daybreak/java8inaction/Java8InActionApplication.java
@@ -0,0 +1,12 @@
+package studygroup.daybreak.java8inaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8InActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8InActionApplication.class, args);
+    }
+}
---
+++ src/test/java/studygroup/daybreak/java8inaction/Java8InActionApplicationTests.java
@@ -0,0 +1,16 @@
+package studygroup.daybreak.java8inaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8InActionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
