067c7a004d9a0e87214c79d6e487fd69184c5176
coalcellar <coalcellar@interpark.com>
2018-08-07 21:51:56 +0900

    #7 7장실습코드

--- src/main/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSeven.java
+++ src/main/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSeven.java
@@ -1,7 +1,45 @@
 package studygroup.daybreak.java8inaction.chap7.yuna;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Function;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 /**
  * author: jihye choi
  */
 public class ChapterSeven {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChapterSeven.class);
+
+    public static long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1).limit(n).reduce(0L, Long::sum);
+    }
+
+    public static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1).limit(n).parallel().reduce(0L, Long::sum);
+    }
+
+    // boxing unboxing 문제, 청크 구분이 안되는 문제 등 parallel의 장점을 살릴 수 없음
+    public static long measureSumPerf(Function<Long, Long> adder, long n) {
+        long fastest = Long.MAX_VALUE;
+        for(int i = 0; i < 10; i++ ) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+//            logger.debug("result : {} ", sum);
+            if(duration < fastest) fastest = duration;
+        }
+        return fastest;
+    }
+
+    public static long rangedSum(long n) {
+        return LongStream.rangeClosed(1, n).reduce(0L, Long::sum);
+    }
+
+    public static long parallelRangedSum(long n) {
+        return LongStream.rangeClosed(1, n).parallel().reduce(0L, Long::sum);
+    }
 }
--- src/test/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSevenTest.java
+++ src/test/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSevenTest.java
@@ -1,17 +1,31 @@
 package studygroup.daybreak.java8inaction.chap7.yuna;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * author: jihye choi
  */
 public class ChapterSevenTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChapterSevenTest.class);
+
+    @Test
+    public void 병렬테스트1() {
+//        logger.debug("sequentialSum : {}", ChapterSeven.sequentialSum(30));
+//        logger.debug("parallelSum : {}", ChapterSeven.parallelSum(30));
+
+//        logger.debug("sequentialSum done in : [ {} ] msecs", ChapterSix.collectorHarness(ChapterSeven::sequentialSum));
+//        logger.debug("parallelSum done in : [ {} ] msecs", ChapterSix.collectorHarness(ChapterSeven::parallelSum));
+
+//        logger.debug("measureSumPerf sequentialSum in [ {} ] msecs", ChapterSeven.measureSumPerf(ChapterSeven::sequentialSum, 10_000_000));
+//        logger.debug("measureSumPerf parallelSum in [ {} ] msecs", ChapterSeven.measureSumPerf(ChapterSeven::parallelSum, 10_000_000));
+
+        logger.debug("rangedSum : {}", ChapterSeven.rangedSum(30));
+        logger.debug("parallelRangedSum : {}", ChapterSeven.parallelRangedSum(30));
+
+        logger.debug("measureSumPerf rangedSum in [ {} ] msecs", ChapterSeven.measureSumPerf(ChapterSeven::rangedSum, 10_000_000));
+        logger.debug("measureSumPerf parallelRangedSum in [ {} ] msecs", ChapterSeven.measureSumPerf(ChapterSeven::parallelRangedSum, 10_000_000));
+    }
 
 }
