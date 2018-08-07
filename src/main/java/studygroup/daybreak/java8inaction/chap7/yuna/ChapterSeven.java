0bcdef7fc65087fa156001f3e8f1412bbad88c2d
coalcellar <coalcellar@interpark.com>
2018-08-07 21:26:03 +0900

    #6 6장실습코드 - first commit

---
+++ src/main/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSeven.java
@@ -0,0 +1,7 @@
+package studygroup.daybreak.java8inaction.chap7.yuna;
+
+/**
+ * author: jihye choi
+ */
+public class ChapterSeven {
+}
---
+++ src/test/java/studygroup/daybreak/java8inaction/chap7/yuna/ChapterSevenTest.java
@@ -0,0 +1,17 @@
+package studygroup.daybreak.java8inaction.chap7.yuna;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * author: jihye choi
+ */
+public class ChapterSevenTest {
+
+}
