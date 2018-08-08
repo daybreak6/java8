33756353b6e344f1d9580f56929bdd9fd416dd18
langve <langve0+github@gmail.com>
2018-08-08 16:30:19 +0900

    Revert "#8 chap7 실습 공유"
    This reverts commit c19d364

--- src/main/java/studygroup/daybreak/java8inaction/chap6/yuna/ChapterSix.java
+++ src/main/java/studygroup/daybreak/java8inaction/chap6/yuna/ChapterSix.java
@@ -1,4 +1,4 @@
-package studygroup.daybreak.java8inaction.chap6.yuna;
+package com.yuna.inaction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/studygroup/daybreak/java8inaction/chap6/yuna/ChapterSixTest.java
+++ src/test/java/studygroup/daybreak/java8inaction/chap6/yuna/ChapterSixTest.java
@@ -1,14 +1,9 @@
 package studygroup.daybreak.java8inaction.chap6.yuna;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static org.junit.Assert.assertEquals;
 
 public class ChapterSixTest {
 
-    Logger logger = LoggerFactory.getLogger(ChapterSixTest.class);
     @Test
     public void 테스트일() {
         assertEquals("pork", ChapterSix.test1());
@@ -41,13 +36,13 @@ public class ChapterSixTest {
 
     @Test
     public void 소수테스트() {
-        logger.debug("isPrime : {}", ChapterSix.partitionPrimes(30));
-        logger.debug("isPrime custom : {}", ChapterSix.partitionPrimesWithCustomCollector(100));
+        logger.debug("isPrime : {}", com.yuna.inaction.ChapterSix.partitionPrimes(30));
+        logger.debug("isPrime custom : {}", com.yuna.inaction.ChapterSix.partitionPrimesWithCustomCollector(100));
     }
 
     @Test
     public void 스피드테스트() {
-        logger.debug("partitioning(partitionPrimes) done in : [ {} ] msecs", ChapterSix.collectorHarness(ChapterSix::partitionPrimes));
-        logger.debug("partitioning(partitionPrimesWithCustomCollector) done in : [ {} ] msecs", ChapterSix.collectorHarness(ChapterSix::partitionPrimesWithCustomCollector));
+        logger.debug("partitioning(partitionPrimes) done in : [ {} ] msecs", com.yuna.inaction.ChapterSix.collectorHarness(com.yuna.inaction.ChapterSix::partitionPrimes));
+        logger.debug("partitioning(partitionPrimesWithCustomCollector) done in : [ {} ] msecs", com.yuna.inaction.ChapterSix.collectorHarness(com.yuna.inaction.ChapterSix::partitionPrimesWithCustomCollector));
     }
 }
